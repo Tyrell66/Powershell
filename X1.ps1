3dbf75804008c978a7fbc878877329f1f1b4b53c
Tyrell66 <3510934+Tyrell66@users.noreply.github.com>
2020-05-01 13:22:23 +0200

    Add files via upload

---
+++ X1.ps1
@@ -0,0 +1,52 @@
+﻿function Get-FragmentationPercent
+{
+    param ($strReport, [bool] $bGetAnalysis=$true)
+    begin
+    {
+        $bInAnalysis = $false
+        $bInDefrag = $false
+        foreach ($line in $strReport)
+        {
+            if ($line -match “Analysis Report”)
+            {
+                $bInDefrag = $false;
+                $bInAnalysis = $true;
+            }
+            if ($line -match “Defragmentation Report”)
+            {
+                $bInAnalysis = $false;
+                $bInDefrag = $true;
+            }
+
+            if ($line -match “(?<percentfragmented>\d+)%\sFragmented\s”)
+            {
+                if (($bInAnalysis -and $bGetAnalysis) -or ($bInDefrag -and !$bGetAnalysis))
+                {
+                    return $matches[“percentFragmented”]
+                }
+            }
+        }
+        return 0
+    }
+}
+
+$dLimit = [double]$args[0]
+foreach ($drive in Get-WMIObject Win32_LogicalDisk -filter "DriveType = 3")
+{
+    $strDriveLetter = $drive.DeviceID
+    $strReport = defrag $strDriveLetter -a
+    $dFragPercent = [double] (Get-FragmentationPercent $strReport)
+
+    if ($dFragPercent -gt $dLimit)
+    {
+        echo “Drive $strDriveLetter defragmenting:”
+        echo “    Before:  $dFragPercent% fragmented.”
+        $strDefragReport = defrag $strDriveLetter
+        $dNewFrag = Get-FragmentationPercent $strDefragReport $false
+        echo “    After:  $dNewFrag% fragmented.”                   
+    }
+    else
+    {   
+        echo “Drive $strDriveLetter does not need defragmented ($dFragPercent% fragmented)”
+    } 
+}
